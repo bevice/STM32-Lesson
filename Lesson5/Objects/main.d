7617caf91dbd212438485013e575887fd4ccdf3f
Nordic Energy <hardware.sitara@gmail.com>
2017-11-20 12:23:33 +0300

    adding lesson №5

---
+++ Lesson5/Objects/main.d
@@ -0,0 +1,16 @@
+.\objects\main.o: Code\src\main.c
+.\objects\main.o: .\CMSIS\inc\stm32f10x.h
+.\objects\main.o: .\CMSIS\inc\core_cm3.h
+.\objects\main.o: E:\Soft\Keil 5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: .\CMSIS\inc\system_stm32f10x.h
+.\objects\main.o: .\FreeRTOS\inc\FreeRTOS.h
+.\objects\main.o: E:\Soft\Keil 5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\main.o: .\Code\inc\FreeRTOSConfig.h
+.\objects\main.o: .\FreeRTOS\inc\projdefs.h
+.\objects\main.o: .\FreeRTOS\inc\portable.h
+.\objects\main.o: .\FreeRTOS\inc\deprecated_definitions.h
+.\objects\main.o: .\FreeRTOS\inc\portmacro.h
+.\objects\main.o: .\FreeRTOS\inc\mpu_wrappers.h
+.\objects\main.o: .\FreeRTOS\inc\task.h
+.\objects\main.o: .\FreeRTOS\inc\list.h
+.\objects\main.o: .\FreeRTOS\inc\queue.h
